b0df37ca8abad1d3fb0e517e0a28441e04fce5b5
luis-galvez <luisegalvezg@gmail.com>
2022-01-28 11:36:36 -0700

    Included required packages.

--- setup.py
+++ setup.py
@@ -6,7 +6,8 @@ import sys
 from setuptools import setup, find_packages
 import versioneer
 
-short_description = "Tool to calculate thermodynamic properties using the superposition approximation of the PES.".split("\n")[0]
+short_description = "Tool to calculate thermodynamic properties using the" +\
+                    "superposition approximation of the PES.".split("\n")[0]
 
 # from https://github.com/pytest-dev/pytest-runner#conditional-requirement
 needs_pytest = {'pytest', 'test', 'ptr'}.intersection(sys.argv)
@@ -46,14 +47,14 @@ setup(
 
     # Additional entries you may want simply uncomment the lines you want and fill in the data
     # url='http://www.my_package.com',  # Website
-    # install_requires=[],              # Required packages, pulls from pip if needed; do not use for Conda deployment
-    # platforms=['Linux',
-    #            'Mac OS-X',
-    #            'Unix',
-    #            'Windows'],            # Valid platforms your code works on, adjust to your flavor
-    # python_requires=">=3.5",          # Python version restrictions
+    install_requires=['numpy'],
+    platforms=['Linux',
+               'Mac OS-X',
+               'Unix',
+               'Windows'],            # Valid platforms your code works on, adjust to your flavor
+    python_requires=">=3.7",          # Python version restrictions
 
-    # Manual control if final package is compressible or not, set False to prevent the .egg from being made
-    # zip_safe=False,
+    # Manual control if final package is compressible or not, set False to
+    # prevent the .egg from being made zip_safe=False,
 
 )
