f9a53580e9a257d99ebb25be6a0188edf0069202
luis-galvez <luisegalvezg@gmail.com>
2022-03-31 15:30:47 -0700

    Update documentation style

--- docs/conf.py
+++ docs/conf.py
@@ -105,9 +105,8 @@ html_theme_options = {'page_width': '1200px',
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
 
-html_css_files = [
-    'css/custom.css',
-]
+def setup(app):
+    app.add_css_file('css/custom.css')
 
 # Custom sidebar templates, must be a dictionary that maps document names
 # to template names.
