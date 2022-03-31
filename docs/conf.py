a1ba974cf22f738a576db8f3c7b76d9ad15d1445
Luis Gálvez <luisegalvezg@gmail.com>
2022-03-31 15:04:24 -0700

    Update documentation format

--- docs/conf.py
+++ docs/conf.py
@@ -98,12 +98,13 @@ html_theme = 'alabaster'
 # documentation.
 #
 html_theme_options = {'body_text_align': 'justify',
-                      'page_width': '1200px'}
+                      'page_width': '1200px',
+                      'sidebar_width': '240px'}
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+html_static_path = []
 
 # Custom sidebar templates, must be a dictionary that maps document names
 # to template names.
